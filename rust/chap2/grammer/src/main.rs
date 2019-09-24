86b6f6b5040a092506f4e65620af68017dd7d36c
natsutan <km1km1km1>
2019-09-24 12:33:07 +0900

    ソース整形

--- rust/chap2/grammer/src/main.rs
+++ rust/chap2/grammer/src/main.rs
@@ -1,19 +1,9 @@
+//https://github.com/norvig/paip-lisp/blob/master/docs/chapter2.md#23-a-rule-based-solution
 use rand::Rng;
+
 #[macro_use]
 extern crate lazy_static;
 extern crate gensym;
-
-/*
-(defparameter *simple-grammer*
-  '((sentence -> (noun-phrase verb-phrase))
-    (noun-phrase -> (Article Noun))
-    (verb-phrase -> (Verb noun-phrase))
-    (Article -> the a)
-    (Noun -> man ball woman table)
-    (Verb -> hit took saw liked))
-  "A grammar for a trivial subset of English")
-*/
-
 
 fn random_elm_idx(i:usize) -> usize {
     let mut rng =  rand::thread_rng();
@@ -21,11 +11,11 @@ fn random_elm_idx(i:usize) -> usize {
 }
 
 macro_rules! _simple_rule {
-    ($gensym:ident, $e1: ident, $e11:tt, $e2: expr) => {
+    ($gensym:ident, $e1: ident, $e2:tt, $e3: expr) => {
         lazy_static! {
             static ref $gensym:Vec<&'static str> = {
                 let mut table:Vec<&'static str> = Vec::new();
-                for s in $e2.split(' ') {
+                for s in $e3.split(' ') {
                     table.push(s);
                 }
                 table
@@ -47,13 +37,13 @@ macro_rules! simple_rule {
 }
 
 macro_rules! composite_rule {
-    ($e1: ident, $e_dummy:tt, $e2: expr, $e3: expr) => {
+    ($e1: ident, $e2:tt, $e3: expr, $e4: expr) => {
         fn $e1() -> Vec<&'static str> {
             let mut n:Vec<&str> = Vec::new();
-            for s in $e2() {
+            for s in $e3() {
                 n.push(s);
             }
-            for s in $e3() {
+            for s in $e4() {
                 n.push(s);
             }
             n
@@ -62,13 +52,24 @@ macro_rules! composite_rule {
 }
 
 
+/*
+(defparameter *simple-grammer*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked))
+  "A grammar for a trivial subset of English")
+*/
 
-composite_rule!(noun_phrase, =>, article, noun);
-composite_rule!(verb_phrase, =>, verb, noun_phrase);
-composite_rule!(sentence,    =>, noun_phrase, verb_phrase);
-simple_rule!(article, =>, "a the");
-simple_rule!(noun,    =>, "man ball woman table");
-simple_rule!(verb,    =>, "hit took saw liked");
+composite_rule!(noun_phrase, ->, article, noun);
+composite_rule!(verb_phrase, ->, verb, noun_phrase);
+composite_rule!(sentence,    ->, noun_phrase, verb_phrase);
+simple_rule!(article, ->, "a the");
+simple_rule!(noun,    ->, "man ball woman table");
+simple_rule!(verb,    ->, "hit took saw liked");
+
 
 fn conv_str(v:Vec<&'static str>) -> String {
     let mut cs:String = "".to_string();
@@ -79,7 +80,7 @@ fn conv_str(v:Vec<&'static str>) -> String {
 }
 
 fn main() {
-    for _ in 1..10  {
+    for _ in 0..10  {
         println!("{}", conv_str(sentence()));
     }
 }
